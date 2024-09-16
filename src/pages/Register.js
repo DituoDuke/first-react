99d7ffaf45b7d5ad2db0a48edc3748573278d221
Dituo <titaoduque@gmail.com>
2024-09-16 00:19:03 -0300

    modified:   src/pages/Register.js

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -5,6 +5,7 @@ import {
   faInfoCircle,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import userEvent from "@testing-library/user-event";
 
 //Constante que usaremos para defenir o necessario para o campo de usuario (minimo 3 digitos e maximo 23) sendo limitado pelos caracteres especificados a baixo
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
@@ -27,6 +28,27 @@ function Register() {
   const [matchValida, setMatchValida] = useState(false);
   const [matchfoco, setMatchFoco] = useState(false);
 
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    nomeRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setNomeValido(USER_REGEX.test(nome));
+  }, [nome]);
+
+  useEffect(() => {
+    setSenhaValida(PWD_REGEX.test(senha));
+    const match = senha === matchSenha;
+    setMatchValida(match);
+  }, [senha, matchSenha]);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [nome, senha, matchSenha]);
+
   return (
     <>
       <section>
@@ -44,12 +66,28 @@ function Register() {
             <input
               type="text"
               id="nome"
-              placeholder="Nome"
               ref={nomeRef}
+              autoComplete="off"
+              placeholder="Nome"
+              aria-invalid={nomeValido ? "false" : "true"}
+              aria-describedby="uinote"
               required
+              onFocus={() => setNomeFoco(true)}
+              onBlur={() => setNomeFoco(false)}
               onChange={(e) => setNome(e.target.value)}
             ></input>
           </div>
+          <p
+            id="uidnote"
+            className={
+              nomeFoco && nome && !nomeValido ? "instructions" : "offscreen"
+            }
+          >
+            <FontAwesomeIcon icon={faInfoCircle} />
+            4 a 24 caracteres <br />
+            Precisa começar com uma letra <br />
+            Letras, numaros, sinais de separação (- e _) são permitidos
+          </p>
 
           <div>
             <label htmlFor="senha">Senha </label>
